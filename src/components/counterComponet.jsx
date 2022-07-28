21d210b3738a84e607abfa10f8d333e40a85d91f
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-07-28 19:34:27 +0530

    Conditional Rendering

--- src/components/counterComponet.jsx
+++ src/components/counterComponet.jsx
@@ -2,42 +2,23 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3", "tag4"],
+    // tags: ["tag1", "tag2", "tag3", "tag4"],
+    tags: [],
   };
-  render() {
-    return (
-      <div className="">
-        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
-        {/* <span>{this.formatCount()}</span> */}
-        <button
-          onClick={this.increamentCount}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There is No Tags</p>;
+    else
+      return (
         <ul>
           {this.state.tags.map((hello) => (
             <li key={hello}>{hello}</li>
           ))}
         </ul>
-      </div>
-    );
+      );
   }
-
-  getBadgeClass() {
-    let classes = "badge m-2 ";
-    classes += this.state.count === 0 ? "bg-warning text-dark" : "bg-primary";
-    return classes;
-  }
-  increamentCount() {
-    const { count } = this.state;
-    count += 1;
-    console.log(count);
-    return count;
-  }
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  render() {
+    return this.renderTags();
   }
 }
 
