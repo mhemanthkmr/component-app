33584d4dafcfa20beee81caf0e9580051e38de58
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-07-28 19:52:41 +0530

    Added A increament and decreament

--- src/components/counterComponet.jsx
+++ src/components/counterComponet.jsx
@@ -1,24 +1,51 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0,
-    // tags: ["tag1", "tag2", "tag3", "tag4"],
-    tags: [],
+    count: 1,
+    tags: ["tag1", "tag2", "tag3", "tg"],
   };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There is No Tags</p>;
-    else
-      return (
-        <ul>
-          {this.state.tags.map((hello) => (
-            <li key={hello}>{hello}</li>
-          ))}
-        </ul>
-      );
-  }
   render() {
-    return this.renderTags();
+    return (
+      <div className="">
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+        {/* <span>{this.formatCount()}</span> */}
+        <button
+          onClick={this.decreamentEvent}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          -
+        </button>
+        <button
+          onClick={this.increamentEvent}
+          className="btn btn-secondary btn-sm"
+        >
+          +
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClass() {
+    let classes = "badge m-2 ";
+    const { count } = this.state;
+    if (count === 0) {
+      classes += "bg-warning text-dark";
+    } else if (count < 0) {
+      classes += "bg-danger";
+    } else {
+      classes += "bg-primary";
+    }
+    return classes;
+  }
+  increamentEvent = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  decreamentEvent = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
